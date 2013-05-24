f1115d72f52044455a1f67feac7c5d3d28677aa1
lacemaker <merlettaia@gmail.com>
2013-05-25 02:12:33 +0400

    +

--- Distances in Trees/solution.rb
+++ Distances in Trees/solution.rb
@@ -7,72 +7,33 @@
 
 
 
-@adj_list = Hash.new
-@marks_list = Hash.new
 
-def fill_adjacensy_list(newick_str)
- a =  newick_str.scan(/(?:(?<f1>\((?:(?>\\[()]|[^()])|\g<f1>)*\)))?(?<f2>\w*)\,?(?:(?<f3>\((?:(?>\\[()]|[^()])|\g<f3>)*\)))?(?<f4>\w*)?/)
- puts a.join('-')
-@adj_list[newick_str] = [] if not @adj_list.has_key? newick_str
-    
-  if not a[1].nil?
-    @adj_list[newick_str] << a[1]
-    @adj_list[a[1]] = [] if not @adj_list.has_key? a[1]
-    @adj_list[a[1]] << newick_str
-    fill_adjacensy_list(arr[0])
-  else
-    @adj_list[newick_str] << a[0]
-    @adj_list[a[0]] = [] if not @adj_list.has_key? a[0]
-    @adj_list[a[0]] << newick_str
-    fill_adjacensy_list(a[0])
-  end
+def get_distance(newick_str, v1, v2)
+  return get_distance(newick_str, v2, v1) if newick_str.index(v1) > newick_str.index(v2)
+  s = newick_str[(newick_str.index(v1) + v1.size)..(newick_str.index(v2)-1)].gsub(/[^\(\)\,]*/,'')
 
- puts a.join(' - ') 
+  s = s.gsub(/(?:(?<f1>\((?:(?>\\[()]|[^()])|\g<f1>)*\)))/,'')
+  puts s
+  r = (s.count(',') > 0 ? 2 : 0) + (s.count('\(') + s.count('\)')).abs
+  puts r
+  r
 end
 
-def bfs(adj_list, marks_list, start_vertex, group_index)
-  a = [start_vertex]
-  marks_list[start_vertex] = group_index
-  while a.size > 0 
-    v = a.shift
-    if adj_list.has_key? v
-      adj_list[v].each do |v2|
-        if marks_list[v2].nil?
-          marks_list[v2] = group_index
-          a.unshift(v2)
-        end
-      end
-    end
-  end
-end
 
-n = 0
+@results = []
 
 while not @infile.eof?
-  @adj_list = Hash.new
-  fill_adjacensy_list(@infile.gets().to_s)
+a = @infile.gets().to_s
   b = @infile.gets().split
   break if b.size < 2
-  #puts @adj_list.keys.join(' ')
-
+  @infile.gets()
   v1 = b[0].to_s
-  v2 = b[1].to_s
-  n = n + 1
+  v2 = b[1].to_s 
+  @results << get_distance(a, v1, v2)
 end
 
-#counter = 0
-#i=0
-#start_key = @adj_list.keys[0]
-#while not i.nil?
-#  counter = counter + 1 
-#  start_key = @adj_list.keys[i] if not i.nil?
+@outfile.puts @results.join(' ')
 
-#  bfs(@adj_list, @marks_list, start_key, counter)
-#  i = @adj_list.keys.find_index{|x| (@marks_list.has_key?(x) ? nil : x) }
- # @outfile.puts @marks_list.keys.keep_if{|x| @marks_list[x] == counter}.join(' ')
-
-#end
-#@outfile.puts "#{counter + (n - @marks_list.keys.count - 1)}" 
 
 @infile.close
 @outfile.close
